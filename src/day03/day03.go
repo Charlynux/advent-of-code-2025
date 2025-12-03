dd8b9c8f6a28f8bdd71e2bee1f9970495dfb51b2
Charles Fourdrignier <charles.fourdrignier@gmail.com>
2025-12-03 20:16:22 +0100

    Day 3 - Part 2 in Go

--- src/day03/day03.go
+++ src/day03/day03.go
@@ -4,27 +4,41 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"math"
 	"os"
 	"strconv"
 	"strings"
 )
 
-func extract_batteries_part1(s string) int {
-	xs := strings.Split(s, "")
+// 3
+// i < maxLen - 2 (3 - 0)
+// i < maxLen - 1 (3 - 1 - 1)
 
-	var currentDec int = -1
-	var currentUnit int = -1
+func extract_batteries_generic(s string, targetLen int) int {
+	xs := strings.Split(s, "")
+	var currentResult = make([]int, targetLen)
+	for i := 0; i < targetLen; i++ {
+		currentResult[i] = -1
+	}
 	maxLen := len(xs)
-	for j := 0; j < maxLen; j++ {
-		currentVal, _ := strconv.Atoi(xs[j])
-		if (currentDec < currentVal) && (j < maxLen-1) {
-			currentDec = currentVal
-			currentUnit = -1
-		} else if currentUnit < currentVal {
-			currentUnit = currentVal
+	for i := 0; i < maxLen; i++ {
+		currentVal, _ := strconv.Atoi(xs[i])
+		for j := 0; j < targetLen; j++ {
+			if currentResult[j] < currentVal && i < maxLen-(targetLen-1-j) {
+				currentResult[j] = currentVal
+				for k := j + 1; k < targetLen; k++ {
+					currentResult[k] = -1
+				}
+				break
+			}
 		}
 	}
-	return currentDec*10 + currentUnit
+	result := 0
+	for i := 0; i < targetLen; i++ {
+		result += currentResult[i] * int(math.Pow(10, float64(targetLen-i-1)))
+	}
+
+	return result
 }
 
 func main() {
@@ -36,6 +50,7 @@ func main() {
 
 	reader := bufio.NewReader(file)
 	result := 0
+	result_part2 := 0
 	for {
 		line, _, err := reader.ReadLine()
 
@@ -43,7 +58,9 @@ func main() {
 			break
 		}
 
-		result += extract_batteries_part1(string(line))
+		result += extract_batteries_generic(string(line), 2)
+		result_part2 += extract_batteries_generic(string(line), 12)
 	}
 	fmt.Printf("%d \n", result)
+	fmt.Printf("%d \n", result_part2)
 }
